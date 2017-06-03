740ee932c9a29bc9a0cd3ca5ec67fabbfe862d9c
Shali Mor <shmor@cisco.com>
2017-06-04 02:03:46 +0300

    modify engine to be as close as possible to the imlementation of python http server

--- kmean-engine.py
+++ kmean-engine.py
@@ -1,14 +1,15 @@
+
 import random
 import cluster
-import s_logger
 
 import json
-import time
 import os
 
 from BaseHTTPServer import HTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 from SocketServer import ThreadingMixIn
+
+
 """
 
 This is a pure Python implementation of the K-Means Clustering algorithmn. The
@@ -41,12 +42,10 @@ def handleKmeanRequest(jsonBody):
 
     else:
         points = [
-        makeRandomPoint( lower, upper) for i in xrange(num_points)
+            makeRandomPoint( lower, upper) for i in xrange(num_points)
         ]
 
-
     clusters = kmeans(points, k, epsilon)
-
 
     # Print our clusters
     for i, c in enumerate(clusters):
@@ -141,22 +140,6 @@ def kmeans(points, k, cutoff  ):
 
 
 
-HOST_NAME = ''  # !!!REMEMBER TO CHANGE THIS!!!
-PORT_NUMBER = int(os.getenv("USER_SIM_PORT", 5555))
-USER_NAME = str(os.getenv("TEST_USER", "test-user"))
-ENABLE_SSL = False
-
-
-class recived_hedaer:
-    name = ""
-    value = ""
-
-
-recived_hedaers = []
-
-# ---------------------- parse command line parameters ------------------------
-
-
 
 # --------------------------------
 def sendResponse(s, status_code, body):
@@ -171,107 +154,58 @@ def sendResponse(s, status_code, body):
 
 def obj_dict(obj):
     return obj.__dict__
+
+
 class MyHandler(SimpleHTTPRequestHandler):
-    def do_HEAD(s):
-        s.protocol_version = "HTTP/1.1"
-        s.send_response(200)
-        s.send_header("Content-type", "text/html")
-        s.end_headers()
-
-
-
-    def do_GET(s):
-        body = ""
-        status_code = 403
-        s_logger.log.info("GOT GET from = " + str(s.client_address))
-        s.protocol_version = "HTTP/1.1"
-        get_operation = s.path.strip('/')
-        s_logger.log.info("required operation: " + get_operation)
-        if (get_operation == "test_connection"):  # test connection
-            body, status_code = "test" ,200
-        elif (get_operation == "test_http"):
-            body, status_code = "test" , 200
-        # elif(get_operation == "userSim/get_num_new_devices"):
-        #     filters = {"discovery_state" : "new"}
-        #     body,status_code = GetNumDevices(filters)
-
-
-        else:
-            status_code = 403
-            body = "Error in url path"
-
-        sendResponse(s, status_code, body)
 
     def do_POST(s):
         body = ""
         status_code = 403
-        s_logger.log.info("GOT POST from = " + str(s.client_address[0]))
+        print("GOT POST from = " + str(s.client_address[0]))
         s.protocol_version = "HTTP/1.1"
         post_operation = s.path.strip('/')
-        s_logger.log.info("required operation: " + post_operation)
+        print("required operation: " + post_operation)
 
         length = int(s.headers.getheader('content-length'))
         cluster_list = []
         if length:
             received_body = s.rfile.read(length)
-            if ("kmean-calculation" in post_operation):
-                res = handleKmeanRequest(received_body)
-                print res
-                if res is None:
-                    status_code = 500
-                    body = "can't calculate kmean"
 
-                else :
-                    body = json.dumps(res,default=obj_dict)
-                    status_code = 200
-            elif (post_operation == "test"):
-                status_code, body = 200, "test accepted"
+            res = handleKmeanRequest(received_body)
+            print res
+            if res is None:
+                status_code = 500
+                body = "can't calculate kmean"
 
-            else:
-                status_code = 403
-                body = "Error in url path"
+            else :
+                body = json.dumps(res,default=obj_dict)
+                status_code = 200
         else:
             status_code = 403
             body = "No length in headers"
+
         sendResponse(s, status_code, (json.dumps(body)))
 
-    # this override will silent (no prints) the BaseHTTPServer
-    def log_message(self, format, *args):
-        return
 
     def do_PUT(s):
-        body = ""
+        body = "Put not supported"
         status_code = 403
-        s_logger.log.debug("GOT PUT from = " + str(s.client_address[0]))
-        s.protocol_version = "HTTP/1.1"
-        put_operation = s.path.strip('/')
-        s_logger.log.debug("required operation: " + put_operation)
 
-        length = int(s.headers.getheader('content-length'))
-        if length:
-            received_body = s.rfile.read(length)
-            if (put_operation == "userSim/clear_ips_notifications"):
-                global num_ips_completed_rules
-                global num_recived_ips_alerts
-
-                num_ips_completed_rules = 0
-                num_recived_ips_alerts = 0
-                status_code = 200
-                body = "ips alerts counters were resest"
-            else:
-                status_code = 403
-                body = "Error in url path"
-        else:
-            status_code = 403
-            body = "No length in headers"
         sendResponse(s, status_code, body)
 
 
-
+    def do_GET(s):
+        body = "Get not supported"
+        status_code = 403
+        sendResponse(s, status_code, body)
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     """Handle requests in a separate thread."""
 
+
+
+HOST_NAME = ''  # !!!REMEMBER TO CHANGE THIS!!!
+PORT_NUMBER = int(os.getenv("KMEAN_PORT", 5555))
 
 
 # ---------------------------------------------------------------------
@@ -280,18 +214,14 @@ class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
 
 if __name__ == '__main__':
 
-    # if(ENABLE_SSL == True):
-    #     server_class = SecureHTTPServer
-    #     httpd = server_class((HOST_NAME, PORT_NUMBER), SecureHTTPRequestHandler)
-    # else:
     server_class = ThreadedHTTPServer
     httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
 
-    s_logger.log.info(time.asctime() + "Server Starts - " + str(HOST_NAME) + ":" + str(PORT_NUMBER))
+    print("Server Starts - " + str(HOST_NAME) + ":" + str(PORT_NUMBER))
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
-        s_logger.log.debug("Ctrl-c received!")  # Sending kill to threads")
+        print("Ctrl-c received!")  # Sending kill to threads")
 
     httpd.server_close()
-    s_logger.log.info(time.asctime() + "Server Stops - " + str(HOST_NAME) + ":" + str(PORT_NUMBER))
+    print("Server Stops - " + str(HOST_NAME) + ":" + str(PORT_NUMBER))
